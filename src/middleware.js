99da915239451dfca87ede39f8953f5f531c6251
Marco Calderon <marco.calderon1701@hotmail.com>
2024-01-31 12:21:28 -0500

    gaa

--- src/middleware.js
+++ src/middleware.js
@@ -2,41 +2,38 @@
 import { NextResponse } from 'next/server';
 import subdomains from './subdomains.json';
 
-
 export const config = {
   matcher: [
     "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
   ],
 };
+
 export default async function middleware(req) {
   const url = req.nextUrl;
   const hostname = req.headers.get("host");
 
   // Define los dominios permitidos (localhost y dominio para producción)
-  // Define allowed Domains (localhost and production domain)
   const allowedDomains = ["localhost:3000", "https://multi-tenant-ten.vercel.app"];
 
   // Verificamos si el hostname existe en los dominios permitidos
-  // Verify if hostname exist in allowed domains
   const isAllowedDomain = allowedDomains.some(domain => hostname.includes(domain));
 
   // Extraemos el posible subdominio en la URL
-  // Extract the possible subdomain in the URL
   const subdomain = hostname.split('.')[0];
 
-  // Si estamos en un dominio habilitado y no es un subdominio, permitimos la solicitud.
-  // If we stay in a allowed domain and its not a subdomain, allow the request.
+  // Si estamos en un dominio permitido y no es un subdominio, permitimos la solicitud.
   if (isAllowedDomain && !subdomains.some(d => d.subdomain === subdomain)) {
     return NextResponse.next();
   }
 
+  // Si el subdominio existe en la lista de subdominios permitidos, reescribimos la URL.
   const subdomainData = subdomains.find(d => d.subdomain === subdomain);
 
   if (subdomainData) {
-    // Rewrite the URL in the dynamic route based in the subdomain
     // Reescribe la URL a una ruta dinámica basada en el subdominio
     return NextResponse.rewrite(new URL(`/${subdomain}${url.pathname}`, req.url));
   }
 
+  // Si no es un dominio permitido o subdominio permitido, devolvemos una respuesta 404.
   return new Response(null, { status: 404 });
 }
